509fecf30363078616fbb302d1aa6874b74a8aa9
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-02-19 02:27:23 +0530

    fixed compilation html css and js code

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,10 @@
 export default function Home() {
-  return <div className="w-full h-[calc(100dvh-60px)] text-white flex flex-col gap-3 justify-center items-center">
-    <h1 className="text-6xl font-bold text-center">Web-D Compiler</h1>
-    <p className="text-gray-500 text-center">Compiler HTML, CSS & Javascript Code.</p> 
-  </div>;
+  return (
+    <div className="w-full h-[calc(100dvh-60px)] text-white flex flex-col gap-3 justify-center items-center">
+      <h1 className="text-6xl font-bold text-center">Web-D Compiler</h1>
+      <p className="text-gray-500 text-center">
+        Compiler HTML, CSS & Javascript Code.
+      </p>
+    </div>
+  );
 }
