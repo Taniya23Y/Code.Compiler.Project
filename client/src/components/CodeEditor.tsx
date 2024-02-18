488f5fae63dd5e3baf6ca4b030fc6b26e40483cd
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-02-18 19:39:27 +0530

    Added more code to client repo

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,38 +1,42 @@
 import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { tags as t } from '@lezer/highlight';
-import { draculaInit } from '@uiw/codemirror-theme-dracula';
-import { loadLanguage} from '@uiw/codemirror-extensions-langs';
+import { tags as t } from "@lezer/highlight";
+import { draculaInit } from "@uiw/codemirror-theme-dracula";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
 import { RootState } from "@/redux/store";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { updateCodeValue } from "@/redux/slices/compilerSlice";
 
 export default function CodeEditor() {
-    const currentLanguage = useSelector(
-        (state: RootState) => state.compilerSlice.currentLanguage
-    );
-  const [value, setValue] = React.useState("console.log('hello world!');");
+  const currentLanguage = useSelector(
+    (state: RootState) => state.compilerSlice.currentLanguage
+  );
+  const fullCode = useSelector(
+    (state: RootState) => state.compilerSlice.fullCode
+  );
+  const dispatch = useDispatch();
 
-  const onChange = React.useCallback((val: string) => {
-    console.log("val:", val);
-    setValue(val);
-  }, []);
+  const onChange = React.useCallback((value: string) => {
+    // console.log("val:", typeof val);
+    // setValue(val);
+
+    dispatch(updateCodeValue(value));
+  }, [dispatch]);
   //   return <div>CodeEditor</div>;
 
   return (
     <CodeMirror
-      value={value}
+      value={fullCode[currentLanguage]}
       height="calc(100vh - 60px - 50px)"
       className="code-editor"
       extensions={[loadLanguage(currentLanguage)!]}
       onChange={onChange}
       theme={draculaInit({
         settings: {
-          caret: '#c6c6c6',
-          fontFamily: 'monospace',
+          caret: "#c6c6c6",
+          fontFamily: "monospace",
         },
-        styles: [
-          { tag: t.comment, color: '#6272a4' },
-        ]
+        styles: [{ tag: t.comment, color: "#6272a4" }],
       })}
     />
   );
--- client/src/redux/slices/compilerSlice.ts
+++ client/src/redux/slices/compilerSlice.ts
@@ -1,33 +1,40 @@
-import{ createSlice } from "@reduxjs/toolkit";
-import{ PayloadAction } from "@reduxjs/toolkit/react";
+import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction } from "@reduxjs/toolkit/react";
 
 // Divide interface
 export interface compilerSliceStateType {
-    html: string,
-    css: string,
-    javascript: string,
-    currentLanguage: "html" | "css" | "javascript";
+  fullCode: {
+    html: string;
+    css: string;
+    javascript: string;
+  };
+  currentLanguage: "html" | "css" | "javascript";
 }
 
 const initialState: compilerSliceStateType = {
-    html: "",
-    css: "",
-    javascript: "",
-    currentLanguage: "html",
+  fullCode: {
+    html: "This is HTML Code.",
+    css: "This is CSS Code.",
+    javascript: "This is Javascript Code.",
+  },
+  currentLanguage: "html",
 };
 
 const compilerSlice = createSlice({
-    name: "compilerSlice",
-    initialState,
-    reducers: {
-        updateCurrentLanguage:(
-            state, 
-            action:PayloadAction<compilerSliceStateType["currentLanguage"]>
-            ) => {
-            state.currentLanguage = action.payload;
-        },
+  name: "compilerSlice",
+  initialState,
+  reducers: {
+    updateCurrentLanguage: (
+      state,
+      action: PayloadAction<compilerSliceStateType["currentLanguage"]>
+    ) => {
+      state.currentLanguage = action.payload;
     },
+    updateCodeValue: ( state, action: PayloadAction<string>) => {
+        state.fullCode[state.currentLanguage] = action.payload;
+    },
+  },
 });
 
 export default compilerSlice.reducer;
-export const { updateCurrentLanguage } = compilerSlice.actions;
+export const { updateCurrentLanguage, updateCodeValue } = compilerSlice.actions;
