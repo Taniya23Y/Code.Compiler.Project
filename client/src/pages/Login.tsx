cd049886d78add9fb3a8f107f62515f1182081ab
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-02-21 02:35:38 +0530

    added Auth in project

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,69 @@
 import "./pageStyle/grid.css";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  userId: z.string(),
+  password: z.string(),
+});
 
 export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      userId: "",
+      password: "",
+    },
+  });
+  function handleLogin(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div className='__login grid-bg w-full h-[calc(100dvh-60px)]'>
-        Login page
+    <div className="__login grid-bg w-full h-[calc(100dvh-60px)] flex justify-center items-center">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleLogin)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="userId"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel>Username</FormLabel> */}
+                <FormControl className="text-white">
+                  <Input placeholder="username or Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                {/* <FormLabel>Username</FormLabel> */}
+                <FormControl>
+                  <Input type="password" placeholder="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
-  )
+  );
 }
