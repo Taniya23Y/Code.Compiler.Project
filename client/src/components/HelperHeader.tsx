fe7dcbb10e9486a21d266e3b51fb36009851c38d
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-02-20 17:20:38 +0530

    added more codes to client repo

--- client/src/components/HelperHeader.tsx
+++ client/src/components/HelperHeader.tsx
@@ -9,18 +9,35 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useDispatch, useSelector } from "react-redux";
-import { compilerSliceStateType, updateCurrentLanguage } from "@/redux/slices/compilerSlice";
+import {
+  compilerSliceStateType,
+  updateCurrentLanguage,
+} from "@/redux/slices/compilerSlice";
 import { RootState } from "@/redux/store";
+import { handleError } from "@/utils/handleError";
+import axios from "axios";
 
 export default function HelperHeader() {
-    const dispatch = useDispatch();
-    const currentLanguage = useSelector(
-        (state: RootState) => state.compilerSlice.currentLanguage
-    );
+  const fullCode = useSelector((state:RootState)=>state.compilerSlice.fullCode) 
+  const handleSaveCode = async () => {
+    try {
+      const response = await axios.post("http://localhost:4000/compiler/save", {
+        fullCode:fullCode,
+      });
+      console.log(response.data);
+    } catch (error) {
+      handleError(error);
+    }
+  };
+  const dispatch = useDispatch();
+  const currentLanguage = useSelector(
+    (state: RootState) => state.compilerSlice.currentLanguage
+  );
   return (
     <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
       <div className="__btn_container flex gap-1">
         <Button
+        onClick={handleSaveCode}
           className="flex justify-between items-center gap-1"
           variant="success"
         >
@@ -37,20 +54,26 @@ export default function HelperHeader() {
       </div>
       <div className="__tab_switcher flex justify-center items-center gap-1">
         <small>Current Language:</small>
-            <Select 
-                defaultValue={currentLanguage} 
-                onValueChange={(value) => dispatch(updateCurrentLanguage(value as compilerSliceStateType["currentLanguage"]))}
-            >
-                <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0">
-                    <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectItem value="html">HTML</SelectItem>
-                    <SelectItem value="css">CSS</SelectItem>
-                    <SelectItem value="javascript">JavaScript</SelectItem>
-                </SelectContent>
-            </Select>
-        </div>
+        <Select
+          defaultValue={currentLanguage}
+          onValueChange={(value) =>
+            dispatch(
+              updateCurrentLanguage(
+                value as compilerSliceStateType["currentLanguage"]
+              )
+            )
+          }
+        >
+          <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="html">HTML</SelectItem>
+            <SelectItem value="css">CSS</SelectItem>
+            <SelectItem value="javascript">JavaScript</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
     </div>
   );
 }
