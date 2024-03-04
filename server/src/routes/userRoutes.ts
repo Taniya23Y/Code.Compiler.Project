30e9e6aa82fb77a8e28350621ccc24439abbac8c
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-03-04 22:44:13 +0530

    Added codes to repo

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { login, signup, logout, userDetails } from "../controllers/userController";
 // import { verifyToken } from "../middlewares/verifyToken";
 import {verifyToken} from "../middlewares/verifyToken";
+import { getMyCodes } from "../controllers/compilerController";
 export const userRoutes = express.Router();
 
 userRoutes.post("/signup", signup);
@@ -9,3 +10,4 @@ userRoutes.post("/login", login);
 userRoutes.post("/logout", logout);
 // middleware function to check the token
 userRoutes.get("/user-details", verifyToken, userDetails);
+userRoutes.get("/my-codes", verifyToken, getMyCodes);
